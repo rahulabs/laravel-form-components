3b36b85c8dabd9f7f84896ab3d69bfc400fc7728
Rahul Oberai <roberoi.abstain@gmail.com>
2024-12-10 18:44:42 +0530

    Update FormCheckbox.php

--- src/Components/FormCheckbox.php
+++ src/Components/FormCheckbox.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ProtoneMedia\LaravelFormComponents\Components;
+namespace RahulAbs\LaravelFormComponents\Components;
 
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Arr;
